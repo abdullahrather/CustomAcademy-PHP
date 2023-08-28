3fd4c699e608036431d11a07023b38f04b509673
Abdullah Rather <abdullahrather19@gmail.com>
2023-08-28 18:34:47 +0500

    fonts locally downloaded

--- tss/header.php
+++ tss/header.php
@@ -25,7 +25,7 @@
     <link rel='stylesheet' id='htmega-keyframes-css' href='wp-content/plugins/ht-mega-for-elementor/assets/css/htmega-keyframes001e.css' type='text/css' media='all' />
     <link rel='stylesheet' id='bootstrap-style-css' href='wp-content/themes/education-insight/assets/css/bootstrapf801.css' type='text/css' media='all' />
     <link rel='stylesheet' id='chld_thm_cfg_parent-css' href='wp-content/themes/education-insight/stylef801.css' type='text/css' media='all' />
-    <link rel='stylesheet' id='education-insight-fonts-css' href='http://fonts.googleapis.com/css?family=Roboto%20Slab%3Awght%40100%3B200%3B300%3B400%3B500%3B600%3B700%3B800%3B900%7CRoboto%3Aital%2Cwght%400%2C100%3B0%2C300%3B0%2C400%3B0%2C500%3B0%2C700%3B0%2C900%3B1%2C300%3B1%2C400%3B1%2C500%3B1%2C700%3B1%2C900' type='text/css' media='all' />
+    <link rel='stylesheet' id='education-insight-fonts-css' href='assets/fonts/css/roboto.css' type='text/css' media='all' />
     <link rel='stylesheet' id='education-insight-style-css' href='wp-content/themes/education-insight-child03/style03a8.css' type='text/css' media='all' />
     <style id='education-insight-style-inline-css' type='text/css'>
         .wrap_figure.fixed {
@@ -55,8 +55,8 @@
     <link rel='stylesheet' id='elementor-post-90-css' href='wp-content/uploads/elementor/css/post-90d10b.css' type='text/css' media='all' />
     <link rel='stylesheet' id='elementor-post-17-css' href='wp-content/uploads/elementor/css/post-17a82b.css' type='text/css' media='all' />
     <link rel='stylesheet' id='elementor-post-3971-css' href='wp-content/uploads/elementor/css/post-3971624c.css' type='text/css' media='all' />
-    <link rel='stylesheet' id='google-fonts-1-css' href='https://fonts.googleapis.com/css?family=Roboto%3A100%2C100italic%2C200%2C200italic%2C300%2C300italic%2C400%2C400italic%2C500%2C500italic%2C600%2C600italic%2C700%2C700italic%2C800%2C800italic%2C900%2C900italic%7CRoboto+Slab%3A100%2C100italic%2C200%2C200italic%2C300%2C300italic%2C400%2C400italic%2C500%2C500italic%2C600%2C600italic%2C700%2C700italic%2C800%2C800italic%2C900%2C900italic&amp;ver=6.3' type='text/css' media='all' />
-    <link rel='stylesheet' id='google-fonts-1-css' href='https://fonts.googleapis.com/css?family=Roboto%3A100%2C100italic%2C200%2C200italic%2C300%2C300italic%2C400%2C400italic%2C500%2C500italic%2C600%2C600italic%2C700%2C700italic%2C800%2C800italic%2C900%2C900italic%7CRoboto+Slab%3A100%2C100italic%2C200%2C200italic%2C300%2C300italic%2C400%2C400italic%2C500%2C500italic%2C600%2C600italic%2C700%2C700italic%2C800%2C800italic%2C900%2C900italic%7CPoppins%3A100%2C100italic%2C200%2C200italic%2C300%2C300italic%2C400%2C400italic%2C500%2C500italic%2C600%2C600italic%2C700%2C700italic%2C800%2C800italic%2C900%2C900italic&amp;ver=6.3' type='text/css' media='all' />
+    <link rel='stylesheet' id='google-fonts-1-css' href='assets/fonts/css/roboto-italic.css' type='text/css' media='all' />
+    <link rel='stylesheet' id='google-fonts-1-css' href='assets/fonts/css/roboto-italic-2.css' type='text/css' media='all' />
     <link rel='stylesheet' id='elementor-icons-shared-0-css' href='wp-content/plugins/elementor/assets/lib/font-awesome/css/fontawesome.min9e0b.css' type='text/css' media='all' />
     <link rel='stylesheet' id='elementor-icons-fa-solid-css' href='wp-content/plugins/elementor/assets/lib/font-awesome/css/solid.min9e0b.css' type='text/css' media='all' />
     <script type='text/javascript' src='wp-includes/js/jquery/jquery.min3088.js' id='jquery-core-js'></script>
